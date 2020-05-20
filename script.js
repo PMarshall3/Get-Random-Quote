3a2900f9585862d2a5befd6c7f2ecefa2c9cf430
Paul Marshall <pmarshal33@gmail.com>
2020-05-19 20:54:59 -0400

    version_3
    refactored CODE

--- script.js
+++ script.js
@@ -10,95 +10,76 @@ project 1 - A Random Quote Generator
  * `quotes` array
 ***/
 //build an array Object of quotes
-var quotes = [
-  {
-    msg:'I think, there for Iam.',
-    creator:' René Descartes',
-    citing:' Princicples of phlosophy, Part 1, article 7, ',
-    year: '1644'
-  },
-  {
-    msg:'The supreme art of war is to subdue the enemy without fighting.',
-    creator:' Sun Tzu',
-    citing:' The art of War',
-    year: '500 BC'
-  },
-  {
-    msg:" Price doesn't make deals, and salary doesn't control your career",
-    creator:' Chris Voss',
-    citing:' Never Split the difference, ',
-    year:'May 17,2016'
-  },
-  {
-    msg:" I say luck is when an opportunity comes along and you're prepared for it.",
-    creator:'Denzel Washington',
-    citing:'Speech, ',
-    year:' 2010-2020'
-  },
-  {
-    msg:" The 40% rule is simple: When your mind is telling you that you're done, that you're exhausted, that you cannot possibly go any further, you're only actually 40% done." ,
-    creator:'David Goggins',
-    citing:'Cant Hurt Me, ',
-    year:'Oct 6,2017'
-  }
-];
-
+var quotes = [{
+	msg: 'I think, there for Iam.',
+	creator: ' René Descartes',
+	citing: ' Princicples of phlosophy, Part 1, article 7, ',
+	year: '1644'
+}, {
+	msg: 'The supreme art of war is to subdue the enemy without fighting.',
+	creator: ' Sun Tzu',
+	citing: ' The art of War',
+	year: '500 BC'
+}, {
+	msg: " Price doesn't make deals, and salary doesn't control your career",
+	creator: ' Chris Voss',
+	citing: ' Never Split the difference, ',
+	year: 'May 17,2016'
+}, {
+	msg: " I say luck is when an opportunity comes along and you're prepared for it.",
+	creator: 'Denzel Washington',
+	citing: 'Speech, ',
+	year: ' 2010-2020'
+}, {
+	msg: " The 40% rule is simple: When your mind is telling you that you're done, that you're exhausted, that you cannot possibly go any further, you're only actually 40% done.",
+	creator: 'David Goggins',
+	citing: 'Cant Hurt Me, ',
+	year: 'Oct 6,2017'
+}];
 /***
  * `getRandomQuote` function
-***/
-function getRandomQuote(){
-  let wisdom;
-  let whoSaidIt;
-  let whereSaid;
-  let whenSaid;
-  let storeData = [];
-  var randomNum = Math.floor(Math.random() * (quotes.length));
-
-       wisdom = quotes[randomNum].msg;
-    whoSaidIt = quotes[randomNum].creator;
-    whereSaid = quotes[randomNum].citing;
-     whenSaid = quotes[randomNum].year;
-
-      storeData.push(wisdom);
-      storeData.push(whoSaidIt);
-      storeData.push(whereSaid);
-      storeData.push(whenSaid);
-
-    return storeData;
+ ***/
+function getRandomQuote() {
+	let wisdom;
+	let whoSaidIt;
+	let whereSaid;
+	let whenSaid;
+	let storeData = [];
+	var randomNum = Math.floor(Math.random() * (quotes.length));
+	wisdom = quotes[randomNum].msg;
+	whoSaidIt = quotes[randomNum].creator;
+	whereSaid = quotes[randomNum].citing;
+	whenSaid = quotes[randomNum].year;
+	storeData.push(wisdom);
+	storeData.push(whoSaidIt);
+	storeData.push(whereSaid);
+	storeData.push(whenSaid);
+	return storeData;
 }
-
 /***
  * `printQuote` function
-***/
-  function printQuote(){
-
-    let msg;
-    let whoSaidit;
-    let whereSaidit;
-    let whenSaidit;
-    let data = getRandomQuote();
-
-    msg = document.getElementsByClassName('quote');
-    msg[0].innerHTML = `${data[0]}`;
-
-    whoSaidit = document.getElementsByClassName('source');
-    whoSaidit[0].textContent = `${data[1]}`
-
-    whereSaidit = document.createElement('span');
-    whereSaidit.setAttribute('class','citation');
-    whereSaidit.textContent = `${data[2]}`;
-    whoSaidit[0].appendChild(whereSaidit);
-
-    whenSaidit = document.createElement('span');
-    whenSaidit.setAttribute('class','source')
-    whenSaidit.textContent = `${data[3]}`;
-    whoSaidit[0].appendChild(whenSaidit);
-
-  }
-
+ ***/
+function printQuote() {
+	let msg;
+	let whoSaidit;
+	let whereSaidit;
+	let whenSaidit;
+	let data = getRandomQuote();
+	msg = document.getElementsByClassName('quote');
+	msg[0].innerHTML = `${data[0]}`;
+	whoSaidit = document.getElementsByClassName('source');
+	whoSaidit[0].textContent = `${data[1]}`
+	whereSaidit = document.createElement('span');
+	whereSaidit.setAttribute('class', 'citation');
+	whereSaidit.textContent = `${data[2]}`;
+	whoSaidit[0].appendChild(whereSaidit);
+	whenSaidit = document.createElement('span');
+	whenSaidit.setAttribute('class', 'source')
+	whenSaidit.textContent = `${data[3]}`;
+	whoSaidit[0].appendChild(whenSaidit);
+}
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
-
+ ***/
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
